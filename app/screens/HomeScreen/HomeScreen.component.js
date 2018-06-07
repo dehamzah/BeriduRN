e324cdfd5e9383f03e0dfb49e249fc6135a4f46e
Dede Hamzah <dehamzah@gmail.com>
2018-06-08 00:25:38 +0700

    update performance click on flatlist

--- app/screens/HomeScreen/HomeScreen.component.js
+++ app/screens/HomeScreen/HomeScreen.component.js
@@ -88,6 +88,7 @@ export default class HomeScreen extends PureComponent {
           data={this.state.sources}
           keyExtractor={this.keyExtractor}
           getItemLayout={this.itemLayout}
+          maxToRenderPerBatch = {1}
         />
       </View>
     );
